9bb0cb26a9dab4f379b7390feb432fde6b9576b0
marilu040701 <126912913+marilu040701@users.noreply.github.com>
2023-04-26 23:40:23 -0300

    Actualizacion

--- ProyectoFinalRamosMaria/FrontEnd/src/environments/enviroment.ts
+++ ProyectoFinalRamosMaria/FrontEnd/src/environments/enviroment.ts
@@ -5,7 +5,8 @@
 export const environment = {
 
   production: false,
-  URL: 'http://localhost:8080/'
+  //URL: 'http://localhost:8080/'
+  URL: 'https://fashionable-pearla-marilu040701.koyeb.app/'
 };
 
 /*
