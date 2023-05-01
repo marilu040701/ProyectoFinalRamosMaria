d61b00e3743ac1e467119c3cdde3067ef1ab0946
marilu040701 <126912913+marilu040701@users.noreply.github.com>
2023-04-30 21:00:50 -0300

    Actualizacion del llamado a la url

--- ProyectoFinalRamosMaria/BackEnd/src/main/java/com/Portfolio/RamosMaria/Controller/CEducacion.java
+++ ProyectoFinalRamosMaria/BackEnd/src/main/java/com/Portfolio/RamosMaria/Controller/CEducacion.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"http://localhost:4200", "https://mariaramosfrontend-5b319.web.app"})
+@CrossOrigin(origins = "https://mariaramosfrontend-5b319.web.app")
 public class CEducacion {
 
     @Autowired
--- ProyectoFinalRamosMaria/BackEnd/src/main/java/com/Portfolio/RamosMaria/Controller/CHys.java
+++ ProyectoFinalRamosMaria/BackEnd/src/main/java/com/Portfolio/RamosMaria/Controller/CHys.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200", "https://mariaramosfrontend-5b319.web.app"})
+@CrossOrigin(origins = "https://mariaramosfrontend-5b319.web.app")
 @RequestMapping("/skill")
 public class CHys {
 
