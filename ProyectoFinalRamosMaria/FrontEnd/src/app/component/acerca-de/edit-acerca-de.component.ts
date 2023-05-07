03e5d44a0294ea559ccaee9b19fbe0e2ffbd008c
marilu040701 <126912913+marilu040701@users.noreply.github.com>
2023-05-07 12:05:38 -0300

    Sin edicion de foto

--- ProyectoFinalRamosMaria/FrontEnd/src/app/component/acerca-de/edit-acerca-de.component.ts
+++ ProyectoFinalRamosMaria/FrontEnd/src/app/component/acerca-de/edit-acerca-de.component.ts
@@ -38,9 +38,9 @@ export class EditAcercaDeComponent implements OnInit{
     });
   }
 
-  uploadImage($event:any){
+  /*/uploadImage($event:any){
     const id = this.activatedRouter.snapshot.params['id'];
     const name = "perfil_" + id;
     this.imageService.uploadImage($event, name)
-  }
+  }/*/
 }
